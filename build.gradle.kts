dcd19e9a26ccbe8e7a85a589230b935206852f67
noonmaru <noonmaru10@gmail.com>
2020-02-17 09:13:22 +0900

    convert to kts

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://papermc.io/repo/repository/maven-public/") //paper
+    maven(url = "https://repo.dmulloy2.net/nexus/repository/public/") //protocollib
+    maven(url = "https://jitpack.io/") //tap, psychic
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8")) //kotlin
+    implementation("junit:junit:4.12") //junit
+    implementation("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT") //paper
+    implementation("com.comphenix.protocol:ProtocolLib:4.5.0") //protocollib
+    implementation("com.github.noonmaru:tap:2.1.7") //tap
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "tap-sample-plugin"
