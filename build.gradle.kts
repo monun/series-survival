4822048b026ff801f1d02bb72f851d3ca667b447
Monun <monun1010@gmail.com>
2021-02-12 07:30:09 +0900

    추가: 명시적인 자바 버전

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
 }
 
 tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }
