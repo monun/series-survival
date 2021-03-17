114aeef8f54aa41b0088337b30fbfc0627aa1757
Monun <monun1010@gmail.com>
2021-03-17 12:28:02 +0900

    :coffin:UNUSED: 사용하지 않는 Wand 아이템 제거
    좀비가 네비게이션으로 사용하던 금괴, 소환 아이템으로 대체되어 사용하지 않음

--- src/main/kotlin/com/github/monun/survival/SurvivalItem.kt
+++ src/main/kotlin/com/github/monun/survival/SurvivalItem.kt
@@ -6,8 +6,6 @@ import org.bukkit.inventory.ItemStack
 object SurvivalItem {
     val vaccine = ItemStack(Material.TOTEM_OF_UNDYING)
 
-    val wandNavigate = ItemStack(Material.GOLD_INGOT)
-
     val wandSpector = ItemStack(Material.EMERALD)
 
     val wandSummon = ItemStack(Material.DIAMOND)
